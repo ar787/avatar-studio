9631aed4b944d891adecf8572de400e0d1389cd6
ar787 <armanhovhannisyan1002@gmail.com>
2026-02-18 22:09:47 +0400

    add: setup vite proxy server

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [react()],
 });
