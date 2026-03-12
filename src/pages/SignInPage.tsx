5898fdce63ef604237b17fee8224dfa7eccec0d0
ar787 <armanhovhannisyan1002@gmail.com>
2026-03-12 20:36:55 +0400

    fix sign-in page's background issue

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -16,6 +16,7 @@ import Link from '@ui/components/Link';
 import { useAuthForm } from '@hooks/useAuthForm';
 import { useSnackbar } from '@hooks/useSnackbar';
 import { useAuth } from '@hooks/useAuth';
+import Box from '@mui/material/Box';
 
 export default function SignInPage() {
   const [toggle, setToggle] = useState(false);
@@ -46,84 +47,89 @@ export default function SignInPage() {
   });
 
   return (
-    <Container
-      disableGutters
+    <Box
       sx={{
-        height: '100vh',
-        minHeight: 'calc(100vh - 74px)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'relative',
-        px: { xs: 2, sm: 3 },
-        py: { xs: 2, sm: 4 },
         background:
           'linear-gradient(0deg, rgba(0,0,0,0.62) 0%, rgba(0,0,0,0.62) 100%), rgb(70,70,70)',
       }}
     >
-      <Paper
+      <Container
+        disableGutters
         sx={{
-          minWidth: 480,
-          p: 4,
-          borderRadius: '24px',
-          backgroundColor: 'rgba(255,255,255,0.05)',
-          border: '1px solid rgba(255,255,255,0.1)',
-          backdropFilter: 'blur(10px)',
-          boxShadow: '0px 8px 32px rgba(0,0,0,0.3)',
+          height: '100vh',
+          minHeight: 'calc(100vh - 74px)',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'relative',
+          px: { xs: 2, sm: 3 },
+          py: { xs: 2, sm: 4 },
         }}
       >
-        <Stack spacing={4}>
-          <Typography variant="h2" color="#fff" textAlign="center">
-            Sign In
-          </Typography>
-
-          <Stack spacing={2}>
-            <TextField
-              placeholder="Email"
-              type="email"
-              value={values.email}
-              helperText={errors.email}
-              error={errors.email.length !== 0}
-              onChange={(e) => setField('email', e.target.value)}
-            />
-
-            <TextField
-              required
-              placeholder="Password"
-              type={toggle ? 'text' : 'password'}
-              value={values.password}
-              helperText={errors.password}
-              error={errors.password.length !== 0}
-              onChange={(e) => setField('password', e.target.value)}
-              slotProps={{
-                input: {
-                  endAdornment: (
-                    <IconButton onClick={() => setToggle((p) => !p)}>
-                      {toggle ? (
-                        <VisibilityOffOutlinedIcon />
-                      ) : (
-                        <VisibilityOutlinedIcon />
-                      )}
-                    </IconButton>
-                  ),
-                },
-              }}
-            />
-          </Stack>
-
-          <Stack spacing={1}>
-            <Button onClick={submit} loading={loading}>
+        <Paper
+          sx={{
+            minWidth: 480,
+            p: 4,
+            borderRadius: '24px',
+            backgroundColor: 'rgba(255,255,255,0.05)',
+            border: '1px solid rgba(255,255,255,0.1)',
+            backdropFilter: 'blur(10px)',
+            boxShadow: '0px 8px 32px rgba(0,0,0,0.3)',
+          }}
+        >
+          <Stack spacing={4}>
+            <Typography variant="h2" color="#fff" textAlign="center">
               Sign In
-            </Button>
-
-            <Typography color="#fff">
-              Do not have an account? <Link to="/sign-up">Sign up</Link>
             </Typography>
-          </Stack>
-        </Stack>
-      </Paper>
 
-      {SnackbarComponent}
-    </Container>
+            <Stack spacing={2}>
+              <TextField
+                placeholder="Email"
+                type="email"
+                value={values.email}
+                helperText={errors.email}
+                error={errors.email.length !== 0}
+                onChange={(e) => setField('email', e.target.value)}
+              />
+
+              <TextField
+                required
+                placeholder="Password"
+                type={toggle ? 'text' : 'password'}
+                value={values.password}
+                helperText={errors.password}
+                error={errors.password.length !== 0}
+                onChange={(e) => setField('password', e.target.value)}
+                slotProps={{
+                  input: {
+                    endAdornment: (
+                      <IconButton onClick={() => setToggle((p) => !p)}>
+                        {toggle ? (
+                          <VisibilityOffOutlinedIcon />
+                        ) : (
+                          <VisibilityOutlinedIcon />
+                        )}
+                      </IconButton>
+                    ),
+                  },
+                }}
+              />
+            </Stack>
+
+            <Stack spacing={1}>
+              <Button onClick={submit} loading={loading}>
+                Sign In
+              </Button>
+
+              <Typography color="#fff">
+                Do not have an account? <Link to="/sign-up">Sign up</Link>
+              </Typography>
+            </Stack>
+          </Stack>
+        </Paper>
+
+        {SnackbarComponent}
+      </Container>
+    </Box>
   );
 }
