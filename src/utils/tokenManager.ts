a1022b6c6b8bdbfad73b68876bc6e458000e25bc
ar787 <armanhovhannisyan1002@gmail.com>
2026-03-18 12:48:52 +0400

    fix token expire bug

--- src/utils/tokenManager.ts
+++ src/utils/tokenManager.ts
@@ -2,16 +2,9 @@ import { auth } from '../services/firebase';
 import { onIdTokenChanged, type User } from 'firebase/auth';
 
 let currentUser: User | null = null;
-let token: string | null = null;
 
-onIdTokenChanged(auth, async (user) => {
+onIdTokenChanged(auth, (user) => {
   currentUser = user;
-
-  if (user) {
-    token = await user.getIdToken();
-  } else {
-    token = null;
-  }
 });
 
 const waitForUser = (): Promise<User | null> => {
@@ -25,13 +18,10 @@ const waitForUser = (): Promise<User | null> => {
 
 export const tokenManager = {
   async getToken(): Promise<string | null> {
-    if (token) return token;
-
     const user = currentUser ?? (await waitForUser());
 
     if (!user) return null;
 
-    token = await user.getIdToken();
-    return token;
+    return await user.getIdToken();
   },
 };
